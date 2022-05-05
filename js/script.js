bfa72f8c9fd270c3343c9eeb3307150a7c324538
Isaac Hussain <isaaclearns58@gmail.com>
2022-05-05 08:39:11 +0300

    js/script: Refactor the playRound() function to make the code cleaner and more readible

--- js/script.js
+++ js/script.js
@@ -17,46 +17,63 @@ function playRound(playerChoice, compChoice) {
     if (playerChoice === compChoice) {
     // return ‘its a draw. Show userScore and compScore’
     return `It's a draw, both chose ${playerChoice}`
-    // else if playerChoice is ‘rock’ and compChoice is ‘paper’
-    } else if (playerChoice === 'rock' && compChoice === 'paper') {
-    // add one to the compScore
-    compScore += 1;
-    // return ‘You lose! Paper beats rock. Show userScore and compScore’
-    return `You lose! Computer chose ${compChoice}.`
-    // else if playerChoice is ‘paper’ and compChoice is ‘rock’
-    } else if (playerChoice === 'paper' && compChoice === 'rock') {
-    // add one to the userScore
-    userScore += 1;
-    // return ‘You win! Paper beats rock. Show userScore and compScore’
-    return `You win! Computer chose ${compChoice}.`
-    // else if playerChoice is ‘paper’ and compChoice is ‘scissors’
-    } else if (playerChoice === 'paper' && compChoice === 'scissors') {
-    // add one to the compScore
-    compScore += 1;
-    // return ‘You lose! scissors beats paper. Show userScore and compScore’
-    return `You lose! Computer chose ${compChoice}.`
-    // else if playerChoice is ‘scissors’ and compChoice is ‘paper’
-    } else if (playerChoice === 'scissors' && compChoice === 'paper') {
-    // add one to the userScore
-    userScore += 1;
-    // return ‘You win! scissors beats paper. Show userScore and compScore’
-    return `You win! Computer chose ${compChoice}.`
-    // else if playerChoice is ‘scissors’ and compChoice is ‘rock’
-    } else if (playerChoice === 'scissors' && compChoice === 'rock') {
-    // add one to the compScore
-    compScore += 1;
-    // return ‘You lose! rock beats scissors. Show userScore and compScore’
-    return `You lose! Computer chose ${compChoice}.`
-    // else if playerChoice is ‘rock’ and compChoice is ‘scissors’
-    } else if (playerChoice === 'rock' && compChoice === 'scissors') {
-    // add one to the userScore
-    userScore += 1;
-    // return ‘You win! rock beats scissors. Show userScore and compScore’
-    return `You win! Computer chose ${compChoice}.`
-    }
+    // // else if playerChoice is ‘rock’ and compChoice is ‘paper’
+    // } else if (playerChoice === 'rock' && compChoice === 'paper') {
+    // // add one to the compScore
+    // compScore += 1;
+    // // return ‘You lose! Paper beats rock. Show userScore and compScore’
+    // return `You lose! Computer chose ${compChoice}.`
+    // // else if playerChoice is ‘paper’ and compChoice is ‘rock’
+    // } else if (playerChoice === 'paper' && compChoice === 'rock') {
+    // // add one to the userScore
+    // userScore += 1;
+    // // return ‘You win! Paper beats rock. Show userScore and compScore’
+    // return `You win! Computer chose ${compChoice}.`
+    // // else if playerChoice is ‘paper’ and compChoice is ‘scissors’
+    // } else if (playerChoice === 'paper' && compChoice === 'scissors') {
+    // // add one to the compScore
+    // compScore += 1;
+    // // return ‘You lose! scissors beats paper. Show userScore and compScore’
+    // return `You lose! Computer chose ${compChoice}.`
+    // // else if playerChoice is ‘scissors’ and compChoice is ‘paper’
+    // } else if (playerChoice === 'scissors' && compChoice === 'paper') {
+    // // add one to the userScore
+    // userScore += 1;
+    // // return ‘You win! scissors beats paper. Show userScore and compScore’
+    // return `You win! Computer chose ${compChoice}.`
+    // // else if playerChoice is ‘scissors’ and compChoice is ‘rock’
+    // } else if (playerChoice === 'scissors' && compChoice === 'rock') {
+    // // add one to the compScore
+    // compScore += 1;
+    // // return ‘You lose! rock beats scissors. Show userScore and compScore’
+    // return `You lose! Computer chose ${compChoice}.`
+    // // else if playerChoice is ‘rock’ and compChoice is ‘scissors’
+    // } else if (playerChoice === 'rock' && compChoice === 'scissors') {
+    // // add one to the userScore
+    // userScore += 1;
+    // // return ‘You win! rock beats scissors. Show userScore and compScore’
+    // return `You win! Computer chose ${compChoice}.`
+    // }
+}
+
+if (
+    (playerChoice === 'rock' && compChoice === 'paper') || 
+    (playerChoice === 'paper' && compChoice === 'scissors') ||
+    (playerChoice === 'scissors' && compChoice === 'rock')
+)   {
+        compScore += 1;
+        return `You lose! Computer chose ${compChoice}.`
+    } else if (
+    (playerChoice === 'paper' && compChoice === 'rock') ||
+    (playerChoice === 'scissors' && compChoice === 'paper') ||
+    (playerChoice === 'rock' && compChoice === 'scissors')
+
+)  {
+        userScore += 1;
+        return `You win! Computer chose ${compChoice}.`
     }
 
-
+}
 
 
 function game(){
@@ -85,4 +102,3 @@ function game(){
 }
 
 game();
-
