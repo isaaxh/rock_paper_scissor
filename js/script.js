8bbe99d99a3c8bd33742a8d9dfee33029b1ec22a
Isaac Hussain <isaaclearns58@gmail.com>
2022-07-18 12:52:49 +0300

    Removes certain unfinished features

--- js/script.js
+++ js/script.js
@@ -4,11 +4,23 @@ const btnScissors = document.querySelector('#scissorsBtn');
 const btnStart = document.querySelector('#startBtn');
 const btnRestart = document.querySelector('#restartBtn');
 const divDisplayResults = document.querySelector('#displayResults')
-
+const displayContent = document.querySelector('.container');
+const menuOptions = document.querySelector('.menu-options');
+const endGameMessage = document.querySelector('#message1');
+const roundWinner = document.querySelector('#message2');
+const playerScoreTracker = document.querySelector('#userScore');
+const compScoreTracker = document.querySelector('#compScore')
+const modal = document.querySelector('.modal');
+const span = document.getElementsByClassName("close")[0];
 const gameOptions = ['rock', 'paper', 'scissors'];
+
+// setting default values 
 let userSelection;
 let userScore = 0;
 let compScore = 0;
+btnRestart.style.display = 'none';
+roundWinner.style.display = 'none';
+
 
 // Functions
 
@@ -22,7 +34,7 @@ function compSelection() {
 
 function playRound(playerChoice, compChoice) {
     if (playerChoice === compChoice) {
-        return `It's a draw, both chose ${playerChoice}`;
+        return `It's a DRAW, both chose ${playerChoice}...`;
     }
 
     if (
@@ -31,10 +43,12 @@ function playRound(playerChoice, compChoice) {
         (playerChoice === 'scissors' && compChoice === 'rock')
     ) {
         compScore += 1;
-        return `You lose this round! Computer chose ${compChoice}.`;
+        compScoreTracker.innerText = compScore
+        return `You LOSE this round! Computer chose ${compChoice}...`;
     } else {
         userScore += 1;
-        return `You win this round! Computer chose ${compChoice}.`;
+        playerScoreTracker.innerText = userScore
+        return `You WIN this round! Computer chose ${compChoice}...`;
     }
 }
 
@@ -56,12 +70,27 @@ function scoreTracker() {
 const checkGameState = () => {
     
     if (userScore >= 5 || compScore >= 5) {
-        console.log('GAME OVER');
+        disableGameOptions();
+        showEndGameMessage ();
+        btnRestart.style.display = 'block';
+        return 
     } else {
         console.log('CONTINUE');
     }
-};
+}
 
+
+
+const showEndGameMessage = () => {
+    endGameMessage.innerText = 'GAME OVER! Click Restart to play again'
+}
+
+const disableGameOptions = () => {
+    btnRock.disabled = true;
+    btnPaper.disabled = true;
+    btnScissors.disabled = true;
+}
+ 
 const isWinner = () => {
     if (userScore >= 5) {
         console.log('Player Wins!!!');
@@ -74,23 +103,22 @@ const handleChoiceClicked = (choice) => {
     userSelection = choice;
     console.log(userSelection);
     compSelection(randomNum());
-    console.log(playRound(userSelection, compSelection()));
+    roundWinner.innerText = playRound(userSelection, compSelection());
+    roundWinner.style.display = 'flex';
     scoreTracker();
     checkGameState();
     isWinner();
 };
 
-btnRock.addEventListener('click', () => handleChoiceClicked('rock'));
-btnPaper.addEventListener('click', () => handleChoiceClicked('paper'));
-btnScissors.addEventListener('click', () => handleChoiceClicked('scissors'));
-// Calling the main loop
-
 function beginGame() {
     let i = 0;
     console.log('game has begun');
-    // while (userScore < 5 && compScore < 5) {
-    // }
+    displayContent.style.display = "flex";
+    menuOptions.style.display = "none";
+    btnRock.addEventListener('click', () => handleChoiceClicked('rock'));
+    btnPaper.addEventListener('click', () => handleChoiceClicked('paper'));
+    btnScissors.addEventListener('click', () => handleChoiceClicked('scissors'));
+
 }
-// document.addEventListener('DOMContentLoaded', beginGame);
 
 btnStart.addEventListener('click', beginGame);
